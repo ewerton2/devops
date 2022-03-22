c478b39fef069df0c2fd8851d6988c96f5949b87
ewerton2 <ewertoncarvalho37@gmail.com>
2022-03-22 20:21:15 -0300

    Primeiro commit

---
+++ projeto2/projeto2_website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Projeto2WebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projeto2_website'
---
+++ projeto2/projeto2_website/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Pergunta(models.Model):
+    enunciado = models.TextField(max_length=140)
+    class  Aluno (models.Mode):
+        
